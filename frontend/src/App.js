4f6d9a32249a4002a242e9746cca07c88a0c0e60
PedroVignotto <pedro.vignotto@hotmail.com>
2019-11-04 21:08:20 -0300

    Configurando Reactotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
+import './config/ReacotronConfig';
+
 import Routes from './routes';
 import history from './services/history';
 
